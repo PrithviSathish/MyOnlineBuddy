5a41e0a19e6b8cef7b6a9aeeaa957a44e2327080
Prithvi <prithvi.mar05@gmail.com>
2021-02-22 13:15:12 +0530

    Added text to speech and removed unnecessary lines from Wikipedia

--- Chumma2.py
+++ Chumma2.py
@@ -21,39 +21,3 @@ except wikipedia.exceptions.PageError as e:
     print("Sorry, the page doesn't exist")
 except:
     print("Please enter a valid word!")
-
-    import_file_path = filedialog.askopenfilename()
-    print(import_file_path)
-    image1 = Image.open(import_file_path)
-    im1 = image1.convert('RGB')
-
-
-browseButton = tk.Button(text="     Select File     ", command=getFile, bg='green', fg='white',
-                         font=('helvetica', 12, 'bold'))
-canvas1.create_window(150, 130, window=browseButton)
-
-
-def convertToPdf():
-    global im1
-
-    export_file_path = filedialog.asksaveasfilename(defaultextension='.pdf')
-    im1.save(export_file_path)
-
-
-saveAsButton = tk.Button(text='Convert to PDF', command=convertToPdf, bg='green', fg='white',
-                         font=('helvetica', 12, 'bold'))
-canvas1.create_window(150, 180, window=saveAsButton)
-
-
-def exitApplication():
-    MsgBox = tk.messagebox.askquestion('Exit Application', 'Are you sure you want to exit the application',
-                                       icon='warning')
-    if MsgBox == 'yes':
-        root.destroy()
-
-
-exitButton = tk.Button(root, text='Exit Application', command=exitApplication, bg='brown', fg='white',
-                       font=('helvetica', 12, 'bold'))
-canvas1.create_window(150, 230, window=exitButton)
-
-root.mainloop()
--- main.py
+++ main.py
@@ -8,11 +8,16 @@ from tkinter import filedialog
 from tkinter import messagebox
 import wikipedia
 import warnings
+from gtts import gTTS
+import os
 
+
+# Initialising root
 root = Tk()
 root.configure(background="gray")
 root.geometry("1000x700")
 root.title("MyOnlineBuddy | Home")
+
 
 # ----- Creating the title -----
 L = Label(root, text="My Online Buddy")
@@ -22,22 +27,31 @@ L.pack()
 
 # ----- Functions -----
 def convert_pdf(event):
+
+    # Get the PDF name
     name = input("Enter the name of the PDF: ")
-    print("Converting to PDF...")
+    
     imagelist = filedialog.askopenfilenames()
-    print(imagelist)
-    pdf = FPDF()
-    # imagelist is the list with all image filenames
-    for image in imagelist:
-        pdf.add_page()
-        pdf.image(image, x=None, y=None, w=0, h=0)
-    pdf.output(f"{name}.pdf", "F")
+    # print(imagelist)
+    print("Converting to PDF...")
+    try:
+        pdf = FPDF()
+        # imagelist is the list with all image filenames
+        for image in imagelist:
+            pdf.add_page()
+            pdf.image(image, x=None, y=None, w=0, h=0)
+        pdf.output(f"{name}.pdf", "F")
 
-    messagebox.showinfo("PDF Converter", "Success!")
+        messagebox.showinfo("PDF Converter | Success", "Success!")
 
+    except:
+        messagebox.showerror("PDF Converter | Error", "Failed! Invalid File types")
+        print("Aborted.")
 
 
 def search_wiki(event):
+
+    # get the query
     q = input("Enter your search: ")
     result = wikipedia.search(q)
 
@@ -51,14 +65,37 @@ def search_wiki(event):
         print(title)
         print("-------------------------------------------------------")
         print(content)
+
     except wikipedia.exceptions.DisambiguationError as e:
         print("Sorry, the word " + q + " has many pages. Did you mean any one of these:")
         print(*e.options, sep = "\n")
+        print("Aborted.")
+
     except wikipedia.exceptions.PageError as e:
         print("Sorry, the page doesn't exist")
+        print("Aborted.")
+
     except:
         print("Please enter a valid word!")
+        print("Aborted.")
 
+
+def text_to_speech(event):
+
+    # get the title and the text input
+    title = input("Enter the title: ")
+    Text = input("Enter your words: ")
+
+    print("converting text to speech...")
+
+    try:
+        language = 'en'
+        speech = gTTS(text=Text, lang=language, slow=False)
+        speech.save(title + ".mp3")
+        os.system("start " + title + ".mp3")
+    except:
+        messagebox.showerror("Text To Speech | Error", "Please enter valid words!")
+  
 
 
 # ----- Creating Background Image ------
@@ -78,6 +115,14 @@ button2TXT = canvas.create_text(595, 475, fill="darkblue", font=("courier", 20),
 
 canvas.tag_bind(button2BG, "<Button-1>", search_wiki)
 canvas.tag_bind(button2TXT, "<Button-1>", search_wiki)
+
+# ----- Text To Speech -----
+button3BG = canvas.create_rectangle(100, 570, 300, 520, fill="grey", outline="grey60")
+button3TXT = canvas.create_text(200, 545, fill="darkblue", font=("courier", 15), text="Text to Speech")
+
+canvas.tag_bind(button3BG, "<Button-1>", text_to_speech)
+canvas.tag_bind(button3TXT, "<Button-1>", text_to_speech)
+
 
 img = PhotoImage(file="Assets/TitlePage.png")
 canvas.create_image(10, 10, anchor=NW, image=img)
